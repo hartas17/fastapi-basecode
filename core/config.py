3073aa4d090fe1ace30ddcac729e5c05d927e562
Manuel Can <manuel@controlhub.com>
2023-08-12 11:35:16 -0600

    Set config

--- core/config.py
+++ core/config.py
@@ -8,8 +8,8 @@ class Config(BaseSettings):
     DEBUG: bool = True
     APP_HOST: str = "0.0.0.0"
     APP_PORT: int = 8000
-    WRITER_DB_URL: str = f"mysql+aiomysql://fastapi:fastapi@localhost:3306/fastapi"
-    READER_DB_URL: str = f"mysql+aiomysql://fastapi:fastapi@localhost:3306/fastapi"
+    WRITER_DB_URL: str
+    READER_DB_URL: str
     JWT_SECRET_KEY: str = "fastapi"
     JWT_ALGORITHM: str = "HS256"
     SENTRY_SDN: str = None
@@ -18,10 +18,13 @@ class Config(BaseSettings):
     REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6379
 
+    class Config:
+        env_file = ".env"
+
 
 class DevelopmentConfig(Config):
-    WRITER_DB_URL: str = f"mysql+aiomysql://root:fastapi@db:3306/fastapi"
-    READER_DB_URL: str = f"mysql+aiomysql://root:fastapi@db:3306/fastapi"
+    WRITER_DB_URL: str
+    READER_DB_URL: str
     REDIS_HOST: str = "redis"
     REDIS_PORT: int = 6379
 
